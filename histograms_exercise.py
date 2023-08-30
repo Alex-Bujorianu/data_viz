32e2388bf6dfeaa46460f994a69bb3645c246515
alex-bujorianu <lxbujorianu@gmail.com>
2023-08-30 17:17:29 +0100

    Committing datasets.

--- histograms_exercise.py
+++ histograms_exercise.py
@@ -22,3 +22,34 @@ def mean(row):
 grades.drop('average', axis='columns', inplace=True)
 grades['average'] = grades[['Module 1 Grade', 'Module 2 Grade', 'Module 3 Grade', 'Module 4 Grade']].apply(mean, axis=1)
 print(grades)
+
+# part three: calculating the gradients of a simple linear regression
+
+advertising = pd.read_csv("advertising.csv")
+x_bar = advertising['TV'].mean()
+y_bar = advertising['Sales'].mean()
+numerator = 0
+for i in range(len(advertising['TV'])):
+    numerator += (advertising['TV'].iloc[i] - x_bar) * (advertising['Sales'].iloc[i] - y_bar)
+denominator = 0
+for i in range(len(advertising['TV'])):
+    denominator += (advertising['TV'].iloc[i] - x_bar)**2
+
+m = numerator / denominator
+print("m is ", m)
+c = y_bar - m*x_bar
+print("c is ", c)
+
+def calculate_gradient_for_column(column_name: str):
+    x_bar = advertising['TV'].mean()
+    y_bar = advertising['Sales'].mean()
+    numerator = 0
+    for i in range(len(advertising[column_name])):
+        numerator += (advertising[column_name].iloc[i] - x_bar) * (advertising['Sales'].iloc[i] - y_bar)
+    denominator = 0
+    for i in range(len(advertising[column_name])):
+        denominator += (advertising[column_name].iloc[i] - x_bar) ** 2
+    return numerator / denominator
+
+print("Gradient of radio ", calculate_gradient_for_column('Radio'))
+print("Gradient of newspaper ", calculate_gradient_for_column('Newspaper'))
